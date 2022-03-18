c6faf33d09904da61dc37b8129e1952a87e648d3
temlabs <temilabinjo@hotmail.co.uk>
2022-03-18 13:21:13 +0000

    completed diaginals kata

---
+++ src/product-of-diagonals.spec.ts
@@ -0,0 +1,42 @@
+import {vectorAdd, reverseVector, getPathThroughDiagonals} from "./product-of-diagonals";
+
+
+test.skip("Vector multiplications work", () =>{
+    expect(vectorAdd([1,5],[[2],[10]])).toStrictEqual([2,50])
+});
+
+test.skip("Column vector can be reversed", () => {
+    expect(reverseVector([[0],[1]])).toStrictEqual([[1],[0]])
+})
+
+const testArray:number[][] =[
+    [ 1,  4, 7,  6,  5],
+    [-3,  2, 8,  1,  3],
+    [ 6,  2, 9,  7, -4],
+    [ 1, -2, 4, -2,  6],
+    [ 3,  2, 2, -4,  7]
+];
+
+const testArray2:number[][] = [
+    [1]
+]
+
+const testArray3:number[][] = [
+    [2,2,2,2],
+    [2,2,2,2],
+    [2,2,2,2],
+    [2,2,2,2]
+]
+
+const testArray4:number[][] = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+test("Zig-zag path is identified", () => {
+    expect(getPathThroughDiagonals(testArray)).toStrictEqual(1098),
+    expect(getPathThroughDiagonals(testArray2)).toStrictEqual(0),
+    expect(getPathThroughDiagonals(testArray3)).toStrictEqual(0),
+    expect(getPathThroughDiagonals(testArray4)).toStrictEqual(-72)
+})
---
+++ src/product-of-diagonals.ts
@@ -0,0 +1,105 @@
+export function getPathThroughDiagonals(inputArray: number[][]):number{
+    const n: number = inputArray.length;
+    let sumOfRightDiagonals:number = 0;
+    let sumOfLeftDiagonals:number = 0;
+    let indexVector: number[] = [0,0] // i,j
+    let incrementVector:number[][] = [[1],[0]]; // start by moving one down
+
+    if(inputArray.length===1){
+        return 0;
+    }
+
+    while(indexVector[0]*indexVector[1] <= (n-1)*(n-1)){
+        const numbersInRightDiagonal:number[] = getNumbersInRightDiagonal(indexVector,inputArray) // get numbers in right diagonal
+        let rightDiagonalProduct = multiplyNumberArray(numbersInRightDiagonal); // get sum of right diagonal
+
+        const leftIndexVector:number[] = [indexVector[0],(indexVector[1]*-1) + (n-1)]; // reflect index vector about the y axis going through middle of array
+        const numbersInLeftDiagonal:number[] = getNumbersInLeftDiagonal(leftIndexVector,inputArray) // get numbers in right diagonal
+        let leftDiagonalProduct = multiplyNumberArray(numbersInLeftDiagonal); // get sum of right diagonal       
+
+        sumOfRightDiagonals = sumOfRightDiagonals + rightDiagonalProduct;
+        sumOfLeftDiagonals = sumOfLeftDiagonals + leftDiagonalProduct; // add it to the sum of diagonals
+        incrementVector = reverseVector(incrementVector);
+        indexVector = vectorAdd(indexVector,incrementVector);
+    }
+
+    return sumOfLeftDiagonals - sumOfRightDiagonals;
+}
+
+function getNumbersInRightDiagonal(indexPair: number[], inputArray: number[][]):number[]{
+    // these are the diagonals going up from bottom left to top right (North-East).
+    const n = inputArray.length;
+    const numbersInDiagonal: number[] = [];
+    const topRowHit = (i:number) => i===0;
+    const rightColumnHit = (j:number) => j===(n-1);
+    const bottomRowHit = (i:number) => i===(n-1);
+    const leftColumnHit = (j:number) => j===0;
+    const northEastVector = [[-1],[1]];
+    while (!topRowHit(indexPair[0]) && !rightColumnHit(indexPair[1])){
+        indexPair = vectorAdd(indexPair,northEastVector);
+    }
+    const edgeIndex = indexPair;
+
+    const southWestVector = [[1],[-1]];
+    while(!bottomRowHit(indexPair[0]) && !leftColumnHit(indexPair[1])){
+        let valueToPush:number = inputArray[indexPair[0]][indexPair[1]]; // get value
+        numbersInDiagonal.push(valueToPush); // add it to the array
+        indexPair = vectorAdd(indexPair,southWestVector); // modify access
+    }
+    let valueToPush:number = inputArray[indexPair[0]][indexPair[1]]; // get last value
+    numbersInDiagonal.push(valueToPush); // add it to the array
+
+    return numbersInDiagonal;
+}
+
+function getNumbersInLeftDiagonal(indexPair: number[], inputArray: number[][]):number[]{
+    // these are the diagonals going up from bottom right to top left (North-West).
+    const n = inputArray.length;
+    const numbersInDiagonal: number[] = [];
+    const topRowHit = (i:number) => i===0;
+    const rightColumnHit = (j:number) => j===(n-1);
+    const bottomRowHit = (i:number) => i===(n-1);
+    const leftColumnHit = (j:number) => j===0;
+    const northWestVector = [[-1],[-1]];
+    while (!topRowHit(indexPair[0]) && !leftColumnHit(indexPair[1])){
+        indexPair = vectorAdd(indexPair,northWestVector);
+    }
+    const edgeIndex = indexPair;
+
+    const southEastVector = [[1],[1]];
+    while(!bottomRowHit(indexPair[0]) && !rightColumnHit(indexPair[1])){
+        let valueToPush:number = inputArray[indexPair[0]][indexPair[1]]; // get value
+        numbersInDiagonal.push(valueToPush); // add it to the array
+        indexPair = vectorAdd(indexPair,southEastVector); // modify access
+    }
+    let valueToPush:number = inputArray[indexPair[0]][indexPair[1]]; // get last value
+    numbersInDiagonal.push(valueToPush); // add it to the array
+
+    return numbersInDiagonal;
+}
+
+function multiplyNumberArray(arrToSum:number[]):number{
+    const initialValue = 1;
+    const sumWithInitial = arrToSum.reduce(
+        (previousValue, currentValue) => previousValue * currentValue,
+        initialValue
+    );
+    return sumWithInitial;
+}
+
+export function vectorAdd(rowVector:number[], scalingVector: number[][]):number[]{
+    // no validation, we assume that vector1 is a*b and vector 2 is c*d and that b=c
+    const answer: number[] = [];
+    rowVector.forEach((value,index) => {
+        answer.push(value+scalingVector[index][0]);
+    });
+    return answer;
+}
+
+export function reverseVector(vectorToReverse: number[][]):number[][]{
+    const answer: number[][] = [];
+    for(let i = vectorToReverse.length -1; i >= 0; i--){
+        answer.push(vectorToReverse[i]);
+    }
+    return answer;
+}
